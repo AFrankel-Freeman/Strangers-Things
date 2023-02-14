fed302619735b4593f0872bce3d6e756585396b4
Ali <ali.frankel@gmail.com>
2023-02-14 13:13:44 -0500

    initial setup working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+import React from "react";
+import {createRoot} from "react-dom/client";
+
+
+
+
+const App = () => {
+    return (
+        <h1>Hello World</h1>
+  
+    )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />)
