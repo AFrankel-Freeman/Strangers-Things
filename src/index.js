8db3e53689bb3245d05aef46f7ab72c0a2319dfb
Ali <ali.frankel@gmail.com>
2023-02-15 13:56:19 -0500

    no errors with axios

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {createRoot} from "react-dom/client";
+// import Register from "./register.js";
+import {HashRouter, Routes, Route} from 'react-router-dom';
+import axios from "axios";
 
-
+const URL='https://strangers-things.herokuapp.com/api/2211-FTB-ET-WEB-AM/posts';
 
 
 const App = () => {
+    const [listOfPosts, setListOfPosts]= React.useState(null);
+
+    React.useEffect(() => {
+        axios
+            .get(URL)
+                .then((response) => {setListOfPosts(response.data);
+                    console.log(response)
+                });
+    }, []
+    );
+    
+    if (!listOfPosts) return null;
+
     return (
-        <h1>Hello World</h1>
-  
+        <div>
+            <h1>Posts:{listOfPosts.posts}</h1>
+     
+       
+        </div>
+       
     )
+        
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(
+    <HashRouter>
+    <App/>
+    </HashRouter>
+);
