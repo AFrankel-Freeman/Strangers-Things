defe297401df47d3359dc2ae2ecad6eacc608351
Ali <ali.frankel@gmail.com>
2023-03-08 11:57:30 -0500

    changed script

--- src/index.js
+++ src/index.js
@@ -22,6 +22,7 @@ const App = () => {
 
 
     return (
+        
         <div className="container">
         <Header isLoggedIn = {isLoggedIn} setIsLoggedIn = {setIsLoggedIn}/>
             <Routes>
